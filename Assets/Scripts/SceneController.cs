0cee28e3c018bacce7382eec1bd2dc77a810b2b9
Kaylee <harimn2004@gmail.com>
2022-04-02 12:23:15 -0400

    fahkoei

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneController : MonoBehaviour
+{
+    public void LoadStory1()
+    { 
+         SceneManager.LoadScene("Story1");
+    }
+
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+    public void LoadLevel2()
+    {
+        SceneManager.LoadScene("Level2");
+    }
+    public void LoadLevel3()
+    {
+        SceneManager.LoadScene("Level3");
+    }
+}
