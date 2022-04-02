e5809a554d01abcdde69ff8e440bd9a93ab60342
Kaylee <harimn2004@gmail.com>
2022-04-02 13:34:32 -0400

    fkaljds

--- Assets/Scripts/ShowButton.cs
+++ Assets/Scripts/ShowButton.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class ShowButton : MonoBehaviour
 {
+    public GameObject button;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -15,4 +16,11 @@ public class ShowButton : MonoBehaviour
     {
         
     }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        button.SetActive(true);
+
+        
+    } 
 }
