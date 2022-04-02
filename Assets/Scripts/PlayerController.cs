f07ed5ab3d5e41fe386b1503e2f811e18d52ce89
MazaArt <84805964+MazaArt@users.noreply.github.com>
2022-04-01 21:01:52 -0400

    ajds

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public Rigidbody2D rb;
+    public int jumpForce = 25;
+    public bool isOnGround = true;
+    public float gravityModifier;
+    public bool hasPowerup = false; 
+    private bool jumpHigh; 
+    private bool destroyEnemy;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector2.right * horizontalInput * Time.deltaTime * speed); 
+        if (Input.GetKeyDown(KeyCode.UpArrow) && isOnGround)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            isOnGround = false;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            rb.AddForce(Vector2.left * 10, ForceMode2D.Impulse);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            rb.AddForce(Vector2.right * 10, ForceMode2D.Impulse);
+        }
+    }
+
+    void KeepInBounds(){
+        if (transform.position.x < -752)
+        {
+            transform.position = new Vector2(-752, transform.position.y);
+        }
+
+        if (transform.position.x > 1346)
+        {
+            transform.position = new Vector2(1346, transform.position.y);
+        }
     }
 }
